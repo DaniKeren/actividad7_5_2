34129ae04b831a40dcab587d107525c9c15d46eb
DaniKeren <curraheexs@gmail.com>
2019-02-07 13:01:13 +0100

    primer paso

--- HolaMundo752/src/holamundo752/HolaMundo752.java
+++ HolaMundo752/src/holamundo752/HolaMundo752.java
@@ -16,6 +16,7 @@ public class HolaMundo752 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("primer paso");
     }
     
 }
